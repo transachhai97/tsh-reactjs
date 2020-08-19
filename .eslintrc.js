a29268fd001f69917f76f12de3b1123e658f2b9b
HaiTS <transachhai97@gmail.com>
2020-08-19 09:53:38 +0700

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        commonjs: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb/hooks',
+    ],
+    plugins: [
+        'react',
+    ],
+    parser: 'babel-eslint',
+    rules: {
+        'no-console': 0,
+        'import/no-unresolved': 0,
+        'react/jsx-filename-extension': [
+            1,
+            {
+                extensions: ['.js', '.jsx'],
+            },
+        ],
+        indent: ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'react/jsx-props-no-spreading': 0,
+    },
+};
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,25 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const counterSlice = createSlice({
-  name: 'counter',
-  initialState: {
-    value: 0,
-  },
-  reducers: {
-    increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    name: 'counter',
+    initialState: {
+        value: 0,
     },
-    decrement: state => {
-      state.value -= 1;
+    reducers: {
+        increment: (state) => {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
+            const newState = { ...state };
+            newState.value += 1;
+            return newState;
+        },
+        decrement: (state) => {
+            const newState = { ...state };
+            newState.value -= 1;
+            return newState;
+        },
+        incrementByAmount: (state, action) => {
+            const newState = { ...state };
+            newState.value += action.payload;
+            return newState;
+        },
     },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
-    },
-  },
 });
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
@@ -28,15 +34,15 @@ export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const incrementAsync = amount => dispatch => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount));
-  }, 1000);
+export const incrementAsync = (amount) => (dispatch) => {
+    setTimeout(() => {
+        dispatch(incrementByAmount(amount));
+    }, 1000);
 };
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = state => state.counter.value;
+export const selectCount = (state) => state.counter.value;
 
 export default counterSlice.reducer;
