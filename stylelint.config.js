ace0b07d7956f7fe858a9dce34023ce7a75f85b3
HaiTS <transachhai97@gmail.com>
2020-08-19 09:37:21 +0700

    stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    extends: [
+        'stylelint-config-airbnb',
+        'stylelint-config-rational-order',
+    ],
+    plugins: [
+        'stylelint-order',
+        'stylelint-config-rational-order/plugin',
+    ],
+    ignoreFiles: [
+        '.eslintrc.js',
+        'config-overrides.js',
+    ],
+    rules: {
+        indentation: 4,
+        'number-leading-zero': 'always',
+        'string-quotes': 'single',
+        'rule-empty-line-before': [
+            'always',
+            {
+                except: [
+                    'first-nested',
+                ],
+            },
+        ],
+        'order/properties-order': [],
+        'plugin/rational-order': [
+            true,
+            {
+                'border-in-box-model': true,
+                'empty-line-between-groups': true,
+            },
+        ],
+    },
+};
