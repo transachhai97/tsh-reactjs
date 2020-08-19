3acaa5c88f7b0c39e24209d61750b6110068d48d
HaiTS <transachhai97@gmail.com>
2020-08-19 11:04:21 +0700

    logger

--- src/app/env.js
+++ src/app/env.js
@@ -1,3 +1,5 @@
 const isDev = process.env.NODE_ENV !== 'production';
 
+export const isLogger = isDev && true;
+
 export default isDev;
--- src/app/middleware.js
+++ src/app/middleware.js
@@ -2,6 +2,16 @@ import { getDefaultMiddleware } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import logger from 'redux-logger';
 
-const middleware = [...getDefaultMiddleware(), logger];
+import { isLogger } from '@/app/env';
+
+const middleware = () => {
+    let newMiddleware = [...getDefaultMiddleware()];
+
+    if (isLogger) {
+        newMiddleware = [...newMiddleware, logger];
+    }
+
+    return newMiddleware;
+};
 
 export default middleware;
