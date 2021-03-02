fc65f164735a5fdb24c9ce60d65f3672df2e7ab2
HaiTS <transachhai97@gmail.com>
2021-03-02 14:26:45 +0700

    update html

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from '@/app/store';
-import * as serviceWorker from '@/serviceWorker';
 import '@/locales/i18n';
 import Toastify from '@/common/components/Toastify/Toastify';
 import CNprogress from '@/common/components/CNprogress/CNprogress';
@@ -36,8 +35,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
